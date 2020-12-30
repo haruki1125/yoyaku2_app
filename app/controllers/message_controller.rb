7ee744e83aa2fbc2f6ef4aa4e81489486205104d
haruki1125 <haruki1125@users.noreply.github.com>
2020-12-30 13:27:20 +0900

    commit

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,4 +1,6 @@
 class MessageController < ApplicationController
+  before_action :set_item, only: [:destroy, :show, :update, :edit]
+
   def index
     @message = Message.new
   end
@@ -8,9 +10,37 @@ class MessageController < ApplicationController
     redirect_to events_path
   end
 
+  
+    def show
+    end
+    
+    def edit
+    end
+
+    def update
+      if @message.update(event_parameter)
+      @message.save
+      redirect_to events_path
+      else
+        render :edit 
+      end
+    end
+
+    def destroy
+      @message.destroy
+      redirect_to events_path
+    end
+
+  
+
   private
 
   def event_parameter
     params.require(:message).permit(:start_time, :message)
   end
+
+  def set_item
+    @message = Message.find(params[:id])
+  end
+
 end
