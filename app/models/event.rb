c545fb11c7a4b1e1661df8c5dd90199771761ab1
haruki1125 <haruki1125@users.noreply.github.com>
2021-01-13 16:24:19 +0900

    commit

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,8 @@
 class Event < ApplicationRecord
   with_options presence: true do
     validates :name
-    validates :start_time
     validates :phone_number
     validates :member
-  end
-  with_options format: {/\A[0-9]+\z/} do
-    validates :phone_number
-    validates :member
+    validates :content
   end
 end
